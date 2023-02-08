49a0be62c102bad8a15165ff6124dd2bc280dcfc
magicaldave <smokydave@icloud.com>
2023-02-07 22:44:39 -0600

    Add: 103-merge_sort.c. Tried to do a multithreaded/tiled version. Unsure if the checker will accept it, but it's bangin'.

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 /*Standard Library*/
-#include <stdlib.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 /*Data Structures*/
 /**
  * struct listint_s - Doubly linked list node
@@ -17,6 +18,16 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+/**
+ * struct merge_sort_args - function arguments for merge_sort when split into a thread
+ *
+ * @array: Array to be sorted by merge_sort
+ * @size: Size of the array to be sorted
+ */
+struct merge_sort_args {
+        int *array;
+        size_t size;
+};
 /*Provided Functions*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
@@ -32,4 +43,9 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quickSort(int *array, int low, int high, size_t size);
 int partition(int *array, int start, int end, size_t size);
+void merge_sort(int *array, size_t size);
+/* 103-merge_sort.c */
+void merge_sort(int *array, size_t size);
+void *merge_sort_thread(void *args);
+void print_array_mt(const int *array, size_t size);
 #endif
