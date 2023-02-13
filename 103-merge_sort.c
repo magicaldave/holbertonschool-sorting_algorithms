4506f15fcc3e1de71f5d85ef7d0896d0271a202a
magicaldave <smokydave@icloud.com>
2023-02-13 11:50:32 -0600

    Edit: stylistic fixes to 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,15 @@
 #include "sort.h"
 #include <stdio.h>
 /* Standard top-down implementation. */
+/**
+ * merge - actually merges the arrays together
+ *
+ * @l: Left half of array
+ * @size_l: Length of left half
+ * @r: Right half of array
+ * @size_r: Length of right half
+ * @tmp: Temporary pointer to store data
+ */
 void merge(int *l, size_t size_l, int *r, size_t size_r, int *tmp)
 {
 size_t i = 0, j = 0, k = 0;
@@ -27,26 +36,37 @@ size_t i = 0, j = 0, k = 0;
 	printf("[Done]: ");
 	print_array(l, size_l + size_r);
 }
-
+/**
+ * mergeSortWrap - Wrapper function to recursively call sort function
+ * @temp: temporary pointer to overwrite
+ * @arr: Array to be sorted by merge_sort
+ * @size: Size of the array to be sorted
+ */
 void mergeSortWrap(int arr[], int *temp, size_t size)
 {
-    size_t midpoint;
-    if (size < 2)
-	    return;
-    midpoint = size / 2;
+	size_t midpoint;
 
-    mergeSortWrap(arr, temp, midpoint);
-    mergeSortWrap(arr + midpoint, temp + midpoint, size - midpoint);
+	if (size < 2)
+		return;
+	midpoint = size / 2;
 
-    merge(arr, midpoint, arr + midpoint, size - midpoint, temp);
+	mergeSortWrap(arr, temp, midpoint);
+	mergeSortWrap(arr + midpoint, temp + midpoint, size - midpoint);
+
+	merge(arr, midpoint, arr + midpoint, size - midpoint, temp);
 }
-
+/**
+ * merge_sort - handles memory allocation and invokes wrapper function
+ * @array: Array to be sorted by merge_sort
+ * @size: Size of the array to be sorted
+ */
 void merge_sort(int *array, size_t size)
 {
 	/* Allocate for a size number of integers */
 	int *temp = malloc(sizeof(int) * size);
+
 	if (!temp)
 		return;
-        mergeSortWrap(array, temp, size);
+	mergeSortWrap(array, temp, size);
 	free(temp);
 }
